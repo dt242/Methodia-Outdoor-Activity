9de4a131eadb4dc8e4e6cbb8d50338d9b29ff9b1
Dani <danieltraykov@gmail.com>
2026-06-22 16:11:23 +0300

    Initial commit: Spring Boot skeleton with Web and Lombok

---
+++ src/test/java/com/academy/outdooractivity/OutdooractivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.outdooractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdooractivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
