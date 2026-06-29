e167d0633abc75852a453f311e4446c5de5be9a8
Dani <danieltraykov@gmail.com>
2026-06-29 23:22:40 +0300

    feat: add notification rules to user request config

--- src/main/java/com/academy/outdooractivity/model/UserRequest.java
+++ src/main/java/com/academy/outdooractivity/model/UserRequest.java
@@ -5,5 +5,6 @@ import java.util.Map;
 public record UserRequest(
         String location,
         int forecastDays,
+        NotificationRule notification,
         Map<String, SportRule> sports
 ) {}
